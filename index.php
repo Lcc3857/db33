9e861d0c7ef53d3a33a72c59c94314ebbf8c5ead
xsw <xsw006007@gmail.com>
2022-06-30 07:59:51 +0800

    index layout complete

--- index.php
+++ index.php
@@ -2,9 +2,30 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>無標題文件</title>
+<title>home</title>
 </head>
 
 <body>
+<table width="100" border="1" cellspacing="1" cellpadding="1" align="center">
+  <tr>
+    <td colspan="4">新增</td>
+  </tr>
+  <tr>
+    <td colspan="4">從1~2共2筆</td>
+  </tr>
+  <tr>
+    <td>ID</td>
+    <td>Name</td>
+    <td>Old</td>
+    <td>Addr</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+
 </body>
 </html>
