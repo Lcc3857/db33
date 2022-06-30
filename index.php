76d7c4f55567eb17bea4c6c2c08742e1ba3db8db
xsw <xsw006007@gmail.com>
2022-06-30 08:06:03 +0800

    useed loop row item

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php require_once('Connections/mySQL33.php'); ?>
 <?php
+$maxRows_indexCordSet = 3;
+$pageNum_indexCordSet = 0;
+if (isset($_GET['pageNum_indexCordSet'])) {
+  $pageNum_indexCordSet = $_GET['pageNum_indexCordSet'];
+}
+$startRow_indexCordSet = $pageNum_indexCordSet * $maxRows_indexCordSet;
+
 mysql_select_db($database_mySQL33, $mySQL33);
 $query_indexCordSet = "SELECT * FROM db3";
-$indexCordSet = mysql_query($query_indexCordSet, $mySQL33) or die(mysql_error());
+$query_limit_indexCordSet = sprintf("%s LIMIT %d, %d", $query_indexCordSet, $startRow_indexCordSet, $maxRows_indexCordSet);
+$indexCordSet = mysql_query($query_limit_indexCordSet, $mySQL33) or die(mysql_error());
 $row_indexCordSet = mysql_fetch_assoc($indexCordSet);
-$totalRows_indexCordSet = mysql_num_rows($indexCordSet);
+
+if (isset($_GET['totalRows_indexCordSet'])) {
+  $totalRows_indexCordSet = $_GET['totalRows_indexCordSet'];
+} else {
+  $all_indexCordSet = mysql_query($query_indexCordSet);
+  $totalRows_indexCordSet = mysql_num_rows($all_indexCordSet);
+}
+$totalPages_indexCordSet = ceil($totalRows_indexCordSet/$maxRows_indexCordSet)-1;
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -26,12 +41,18 @@ $totalRows_indexCordSet = mysql_num_rows($indexCordSet);
     <td>Old</td>
     <td>Addr</td>
   </tr>
+  
+  
+  <?php do { ?>
   <tr>
     <td><?php echo $row_indexCordSet['ID']; ?></td>
     <td><?php echo $row_indexCordSet['Name']; ?></td>
     <td><?php echo $row_indexCordSet['Old']; ?></td>
     <td><?php echo $row_indexCordSet['Addr']; ?></td>
   </tr>
+  <?php } while ($row_indexCordSet = mysql_fetch_assoc($indexCordSet)); ?>
+  
+  
 </table>
 
 </body>
