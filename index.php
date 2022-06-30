c643b3a3e232fbc2562f43ced370c667926ad44d
xsw <xsw006007@gmail.com>
2022-06-30 08:03:16 +0800

    create data recordset of index

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<?php require_once('Connections/mySQL33.php'); ?>
+<?php
+mysql_select_db($database_mySQL33, $mySQL33);
+$query_indexCordSet = "SELECT * FROM db3";
+$indexCordSet = mysql_query($query_indexCordSet, $mySQL33) or die(mysql_error());
+$row_indexCordSet = mysql_fetch_assoc($indexCordSet);
+$totalRows_indexCordSet = mysql_num_rows($indexCordSet);
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -20,12 +27,15 @@
     <td>Addr</td>
   </tr>
   <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td><?php echo $row_indexCordSet['ID']; ?></td>
+    <td><?php echo $row_indexCordSet['Name']; ?></td>
+    <td><?php echo $row_indexCordSet['Old']; ?></td>
+    <td><?php echo $row_indexCordSet['Addr']; ?></td>
   </tr>
 </table>
 
 </body>
 </html>
+<?php
+mysql_free_result($indexCordSet);
+?>
